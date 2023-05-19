6b48a66cccd6cd193753354d2eef44f1ff983001
elenavshutova <elenashutova01@gmail.com>
2023-05-19 15:53:50 +0300

    correct

--- lesson1/lesson_1_task_8.py
+++ lesson1/lesson_1_task_8.py
@@ -1,6 +1,14 @@
-def num():
-    num = input("Введите цифру: ")
-    print()
+def my_func(num):
+    print(num, end='')
 
-for i in range(11):
-    num()
+my_func(8)
+my_func(8)
+my_func(0)
+my_func(0)
+my_func(5)
+my_func(5)
+my_func(5)
+my_func(3)
+my_func(5)
+my_func(3)
+my_func(5)
